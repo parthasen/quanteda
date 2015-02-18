27d21173acc1fcc2044cfba0398a474b07fca0f3
Kohei Watanabe <watanabe.kohei@gmail.com>
2015-02-18 07:51:27 +0000

    C++ implementation of tokenize()

---
+++ R/tokenize_c.R
@@ -0,0 +1,88 @@
+rm(list = ls())
+setwd("/home/kohei/Documents/R")
+
+#devtools::install_github("kbenoit/quanteda", ref="dev")
+#devtools::install_github("kbenoit/quantedaData")
+require(quanteda)
+require(quantedaData)
+#install.packages('Rcpp')
+require(Rcpp)
+Sys.setenv("PKG_LIBS"="-lpcrecpp")
+Rcpp::sourceCpp('tokenize.cpp')
+
+# Wrapper -----------------------------------
+tokenize_c <- function(x, sep=' ', simplify=FALSE,
+                      minLength=1, toLower=TRUE, removeDigits=TRUE, removePunct=TRUE,
+                      removeTwitter=TRUE, removeURL=TRUE, removeAdditional=NULL){
+    remove <- c()
+    if(removeDigits){
+      remove <- c(remove,'[[:digit:]]')
+    }
+    if(removePunct){
+      remove <- c(remove,'[[:punct:]]')
+    }
+    if(!is.null(removeAdditional)){
+      remove <- c(remove, removeAdditional)
+    }
+    if(simplify){
+      return(tokenizecpp(x, sep, paste0(remove, collapse='|'), 
+                         minLength, toLower, removeTwitter, removeURL))
+    }else{
+      return(list(tokenizecpp(x, sep, paste0(remove, collapse='|'), 
+                         minLength, toLower, removeTwitter, removeURL)))
+    
+    }
+  
+}
+
+# Dev -----------------------------------------
+
+text1 <- 'This is 1 sentence with 2.0 numbers in it, and one comma.'
+text2 <- "We are his Beliebers, and him is #ourjustin @justinbieber we luv u"
+text2 <- "We are his Beliebers, and him is @justinbieber"
+text3 <- "Collocations can be represented as inheritance_tax using the _ character."
+text4 <- "But under_scores can be removed with removeAdditional."
+text5 <- "This is a $1,500,000 budget and $20bn cash and a $5 cigar"
+text6 <- "URL regex from http://daringfireball.net/2010/07/improved_regex_for_matching_urls."
+
+tokenize(text1, removeDigits=FALSE)
+tokenize_c(text1, removeDigits=FALSE)
+tokenize(text1, toLower=FALSE)
+tokenize_c(text1, toLower=FALSE)
+tokenize(text2, removeTwitter=TRUE)
+tokenize_c(text2, removeTwitter=TRUE)
+tokenize(text4, removeAdditional="[_]")
+tokenize_c(text4, removeAdditional="[_]")
+tokenize(text5, removeDigits=FALSE)
+tokenize_c(text5, removeDigits=FALSE)
+tokenize(text6, removeURL=TRUE)
+tokenize_c(text6, removeURL=TRUE)
+
+
+text7 <- as.character('Вчера в Минске завершились переговоры Путина, Меркель, Олланда и Порошенко по вопросу урегулирования конфликта на Украине. По сути, стороны не пришли к каким-то значимым договоренностям. Порошенко зажат между Дебальцевским котлом и Обамой, а Россия уступать не намерена. Об этом в эфире видеоканала Pravda.Ru рассказывает политолог Сергей Михеев.')
+text8 <- as.character('A very common operation with strings, is to tokenize it with a delimiter of your own choice. This way you can easily split the string up in smaller pieces, without fiddling with the find() methods too much. In C, you could use strtok() for character arrays, but no equal function exists for strings. This means you have to make your own. Here is a couple of suggestions, use what suits your best.')
+
+tokenize(text7, removePunct = TRUE)
+tokenize_c(text7, removePunct = TRUE)
+
+tokenize(text8, removePunct = TRUE)
+tokenize_c(text8, removePunct = TRUE)
+
+# Bnchmark--------------------
+
+#install.packages('microbenchmark')
+library(microbenchmark)
+
+text_bech <- text8
+#text_bech <- paste(inaugTexts, collapse=' ')
+#text_bech <- inaugTexts[2]
+microbenchmark(strsplit(text_bech, ' ', fixed=TRUE),
+               tokenizecpp(text_bech, ' ', remove="[^a-zA-Z ]", minLength=1, 
+                           toLower=TRUE, removeTwitter=TRUE, removeURL=TRUE),
+               tokenize_c(text_bech, simplify=TRUE, removeDigits=TRUE, removePunct=TRUE,
+                          toLower=TRUE, removeAdditional='[_]', 
+                          removeTwitter=TRUE, removeURL=TRUE), 
+               tokenize(text_bech, simplify=TRUE, removeDigits=TRUE, removePunct=TRUE,
+                        toLower=TRUE, removeAdditional='[_]', 
+                        removeTwitter=TRUE, removeURL=TRUE), times=1000)
+
---
+++ src/tokenize.cpp
@@ -0,0 +1,75 @@
+#include <Rcpp.h>
+#include <string>
+#include <pcrecpp.h>
+
+using namespace Rcpp;
+using namespace std;
+
+// [[Rcpp::export]]
+Rcpp::CharacterVector tokenizecpp(SEXP x, SEXP sep, SEXP remove,
+                       SEXP minLength,
+                       SEXP toLower, 
+                       SEXP removeTwitter,
+                       SEXP removeURL){
+  
+  std::string str = Rcpp::as <string> (x); 
+  std::string delim = Rcpp::as <string> (sep);
+  std::string pat_rm = Rcpp::as <string> (remove); 
+  const char *delim_char = delim.c_str();
+  
+  // Options for the future
+  int len_min = Rcpp::as <int> (minLength);
+  bool to_lower = Rcpp::as <bool> (toLower);
+  bool rm_twitter = Rcpp::as <bool> (removeTwitter);
+  bool rm_url = Rcpp::as <bool> (removeURL);
+  
+  // Regexp cleaning
+  if(rm_twitter){
+    pcrecpp::RE re_twitter("(^|\\s)(#|@)\\S+");
+    re_twitter.GlobalReplace(" ", &str);
+  }
+  if(rm_url){
+    pcrecpp::RE re_url("(?i)\\b((?:[a-z][\\w-]+:(?:/{1,3}|[a-z0-9%])|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:\\'\".,<>?]))");
+    re_url.GlobalReplace("", &str);
+  }
+  
+  try{
+    pcrecpp::RE re(pat_rm);
+    re.GlobalReplace("", &str);
+  }catch(std::exception& e){
+    Rcout << "Invalid regular expression given: " <<  pat_rm << "\n";
+  }
+  
+  
+  int len_str = str.length();
+  int i_pos = 0;
+  int i_len = 0;
+  bool flag_token = false;
+  std::string token;
+  std::vector <std::string> tokens;
+  
+  //Rcout << "String: '" << str << "'\n";
+  for(int i=0; i <= len_str; i++){
+    if(to_lower) str[i] = tolower(str[i]);
+    //Rcout << i << ": " << i_pos << " " << i_len << " " << flag_token << " " << str[i] << "\n";
+    i_len = i - i_pos;
+    //if(!std::isalpha(str[i], loc) || str[i] == delim_char[0] || i == len_str){
+    if(str[i] == delim_char[0] || i == len_str){
+      token = str.substr(i_pos, i_len);
+      
+      if(flag_token){
+        if(i_len >= len_min){
+          tokens.push_back(token);
+          //Rcout << i << ": " << i_pos << "-" << i - 1 << " " << token << "\n";
+        }
+        flag_token = false;
+      }
+    }else{
+      if(!flag_token){
+        i_pos = i;
+        flag_token = true;
+      }
+    }
+  }    
+  return wrap(tokens);
+}
