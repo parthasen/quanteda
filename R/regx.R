d41ab28aafaab9ec65c98a09919af3679bb1ae20
koheiw <watanabe.kohei@gmail.com>
2015-03-20 17:34:06 +0000

    Separate files

--- R/regx.R
+++ R/regx.R
@@ -1,4 +1,29 @@
+library(Rcpp)
 
-Sys.setenv("PKG_LIBS"="")
-Sys.setenv("PKG_CPPFLAGS"="-std=gnu++0x")
-Rcpp::sourceCpp('src/regx.cpp')
+Sys.setenv("PKG_CPPFLAGS"="-std=c++0x")
+sourceCpp(code = '
+  // regex_search example
+  #include <iostream>
+  #include <string>
+  #include <regex>
+  
+  int main ()
+  {
+    std::string s ("this substandard sandwich subject has a submarine as a subsequence");
+    std::smatch m;
+    //std::regex e ("\\b(sub)([^ ]*)");   // matches words beginning by "sub"
+    std::regex e ("(^|\\s)(#|@)\\S+", std::regex::ECMAScript);   // matches words beginning by "sub"
+    
+    std::cout << "Target sequence: " << s << std::endl;
+    std::cout << "Regular expression: /\\b(sub)([^ ]*)/" << std::endl;
+    std::cout << "The following matches and submatches were found:" << std::endl;
+    
+    while (std::regex_search (s,m,e)) {
+      for (auto x:m) std::cout << x << " ";
+      std::cout << std::endl;
+      s = m.suffix().str();
+    }
+    
+    return 0;
+  }
+')
